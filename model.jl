eddee7e38561a2f0855e1ae8e52c7d3d70c0c862
Anzury <worstjgl@gmail.com>
2023-02-04 14:03:07 +0100

    modified:   model.jl

--- model.jl
+++ model.jl
@@ -10,26 +10,24 @@ using JuMP
     free variables.
 
     # Arguments
-    - `solver`: the solver Optimizer function.
-    - `instanceMILP`: the MILP instance.
-    - **optional** `bound`: upper bound of m. By default, m = n.
-    - **optional** `P1`: true if modeling with P1 (P2 otherwise and by default).
+    - `instance`: the MILP instance.
     - **optional** `binary`: true if modeling with binary variables (free variables otherwise).
+    - **optional** `solver`: the solver Optimizer function.
 """
-function modelMILP(instanceMILP, binary::Bool = true, solver = Gurobi.Optimizer)
+function modelMILP(instance, binary::Bool = true, solver = Gurobi.Optimizer)
     # Création du model
     m = direct_model((solver)())
     set_optimizer_attribute(m, "LogToConsole", 0)
 
-    X = instanceMILP.X
-    Lmax = instanceMILP.Lmax
-    Lmin = instanceMILP.Lmin
-    B = instanceMILP.B
-    R = instanceMILP.R
-    V = instanceMILP.V 
-    O = instanceMILP.O 
-    Oj = instanceMILP.Oj 
-    U = instanceMILP.U 
+    X = instance.X
+    Lmax = instance.Lmax
+    Lmin = instance.Lmin
+    B = instance.B
+    R = instance.R
+    V = instance.V 
+    O = instance.O 
+    Oj = instance.Oj 
+    U = instance.U 
 
     # Définition des variables
     @variable(m, 0 <= X <= 1, binary=binary)
@@ -56,7 +54,7 @@ function modelMILP(instanceMILP, binary::Bool = true, solver = Gurobi.Optimizer)
 
     list3 = []
     for r in 1:R
-        Brem = B[r]
+        Brem = deepcopy(B[r])
         pop!(Brem)
         for j in Brem
             push!(list3,(j,r))
@@ -67,9 +65,18 @@ function modelMILP(instanceMILP, binary::Bool = true, solver = Gurobi.Optimizer)
     allr = [j for (i,j) in list]
 
     # Définition des contraintes
-    @constraint(m, c2[[(j,r) = t for t in list]], sum(X[r][j][k] for k in Oj[r][j].debut:Oj[r][j].fin) == 1 ) # 2
-    @constraint(m, c3[[(k,r) = t for t in list2]], sum(X[r][j][k] for j in U[r][k]) <= 1 ) # 3
-    @constraint(m, c4[[(j,r) = t for t in list3]], sum(k * X[r][j][k] for k in Oj[r][j].debut:Oj[r][j].fin) <= sum(k * X[r][j+1][k] for k in Oj[r][j+1].debut:Oj[r][j+1].fin)) # 4
+    # @constraint(m, c2[[(j,r) for (j,r) in list]], sum(X[r][j][k] for k in Oj[r][j].debut:Oj[r][j].fin) == 1 ) # 2
+    for (j,r) in list
+        @constraint(m, c2, sum(X[r][j][k] for k=Oj[r][j].debut:Oj[r][j].fin) == 1 ) # 2
+    end
+    # @constraint(m, c3[[(k,r) for (k,r) in list2]], sum(X[r][j][k] for j in U[r][k]) <= 1 ) # 3
+    for (k,r) in list2
+        @constraint(m, c3, sum(X[r][j][k] for j in U[r][k]) <= 1 ) # 3
+    end
+    # @constraint(m, c4[[(j,r) for (j,r) in list3]], sum(k * X[r][j][k] for k in Oj[r][j].debut:Oj[r][j].fin) <= sum(k * X[r][j+1][k] for k in Oj[r][j+1].debut:Oj[r][j+1].fin)) # 4
+    for (j,r) in list3
+    @constraint(m, c4, sum(k * X[r][j][k] for k in Oj[r][j].debut:Oj[r][j].fin) <= sum(k * X[r][j+1][k] for k in Oj[r][j+1].debut:Oj[r][j+1].fin)) # 4
+    end
     @constraint(m, c5[k=1:O], Lmin <= sum(sum(V[r].batch[j] * X[r][j][k] for j in U[r][k]) for r in 1:R) <= Lmax) # 5
 
     return m
