ae58f4a07dc6171acd800713ac277fc3492a2b87
Anzury <worstjgl@gmail.com>
2023-02-05 12:45:14 +0100

    modified:   model.jl 	new file:   newmodel.jl 	modified:   results/12_20_[1,6]_1700_1.txt 	modified:   results/12_20_[1,6]_1700_10.txt 	modified:   results/12_20_[1,6]_1700_11.txt 	modified:   results/12_20_[1,6]_1700_12.txt 	modified:   results/12_20_[1,6]_1700_13.txt 	modified:   results/12_20_[1,6]_1700_14.txt 	modified:   results/12_20_[1,6]_1700_15.txt 	modified:   results/12_20_[1,6]_1700_16.txt 	modified:   results/12_20_[1,6]_1700_17.txt 	modified:   results/12_20_[1,6]_1700_18.txt 	modified:   results/12_20_[1,6]_1700_19.txt 	modified:   results/12_20_[1,6]_1700_2.txt 	modified:   results/12_20_[1,6]_1700_20.txt 	modified:   results/12_20_[1,6]_1700_21.txt 	modified:   results/12_20_[1,6]_1700_22.txt 	modified:   results/12_20_[1,6]_1700_23.txt 	modified:   results/12_20_[1,6]_1700_24.txt 	modified:   results/12_20_[1,6]_1700_25.txt 	modified:   results/12_20_[1,6]_1700_26.txt 	modified:   results/12_20_[1,6]_1700_27.txt 	modified:   results/12_20_[1,6]_1700_28.txt 	modified:   results/12_20_[1,6]_1700_29.txt 	modified:   results/12_20_[1,6]_1700_3.txt 	modified:   results/12_20_[1,6]_1700_30.txt 	modified:   results/12_20_[1,6]_1700_4.txt 	modified:   results/12_20_[1,6]_1700_5.txt 	modified:   results/12_20_[1,6]_1700_6.txt 	modified:   results/12_20_[1,6]_1700_7.txt 	modified:   results/12_20_[1,6]_1700_8.txt 	modified:   results/12_20_[1,6]_1700_9.txt

--- newmodel.jl
+++ newmodel.jl
@@ -1,7 +1,7 @@
 using JuMP
 
 """
-    modelMILP(instance,binary,solver)
+    modelMILP(solver, instance[, P1, binary])
 
     Create the MILP model corresponding to _instance_ using
     `solver` as the optimizer. When `P1` is true we model against problem P1 (see
@@ -19,7 +19,6 @@ function modelMILP(instanceMILP, binary::Bool = true, solver = Gurobi.Optimizer)
     m = direct_model((solver)())
     set_optimizer_attribute(m, "LogToConsole", 0)
     set_optimizer_attribute(m, "MIPFocus", 3)
-    set_optimizer_attribute(m, "TimeLimit", 600)
 
     X = instanceMILP.X
     Lmax = instanceMILP.Lmax
@@ -36,25 +35,18 @@ function modelMILP(instanceMILP, binary::Bool = true, solver = Gurobi.Optimizer)
         maxLenB = max(maxLenB, length(bs))
     end
 
-    copyB = []
-    for r in 1:R
-        Brem = deepcopy(B[r])
-        pop!(Brem)
-        push!(copyB,Brem)
-    end
-
     # Définition des variables
     @variable(m, 0 <= X[r=1:R, j=1:length(B[r]), 1:Oj[r][j].fin] <= 1, binary=binary)
     @variable(m, 0 <= Lmax)
     @variable(m, 0 <= Lmin)
 
-    # Définition de l'objectif (la différence entre charges de mail batch à minimiser)
+    # Définition de l'objectif (la somme des yj à minimiser)
     @objective(m, Min, Lmax - Lmin)
 
     # Définition des contraintes
-    @constraint(m, c2[r=1:R,j in B[r]], sum(X[r, j, k] for k in Oj[r][j].debut:Oj[r][j].fin) == 1 )
+    @constraint(m, c2[r=1:R,j=1:length(B[r])], sum(X[r, j, k] for k in Oj[r][j].debut:Oj[r][j].fin) == 1 )
     @constraint(m, c3[r=1:R,k=1:O], sum(X[r, j, k] for j in U[r][k]) <= 1 )
-    @constraint(m, c4[r=1:R,j in copyB[r]], sum(k * X[r, j, k] for k in Oj[r][j].debut:Oj[r][j].fin) <= sum(k * X[r, j+1, k] for k in Oj[r][j+1].debut:Oj[r][j+1].fin))
+    @constraint(m, c4[r=1:R,j=1:length(B[r])-1], sum(k * X[r, j, k] for k in Oj[r][j].debut:Oj[r][j].fin) <= sum(k * X[r, j+1, k] for k in Oj[r][j+1].debut:Oj[r][j+1].fin))
     @constraint(m, [k=1:O], Lmin <= sum(sum(V[r].batch[j] * X[r, j, k] for j in U[r][k]) for r in 1:R))
     @constraint(m, [k=1:O], sum(sum(V[r].batch[j] * X[r, j, k] for j in U[r][k]) for r in 1:R) <= Lmax)
 
